c81b04b2fb1911d4d5eec5d6b9fc7e903b8ff289
Maxime St-Pierre <me@maximest-pierre.me>
2019-08-25 01:33:27 -0400

    Add handlebars to the mix

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let exphbs = require('express-handlebars');
+
+let app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
